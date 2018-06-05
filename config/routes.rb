52ff953e9d8b9eee82d28ff63d64e9a624dc51ac
Jacques le Roux <nerdy.gninja@gmail.com>
2018-06-05 20:29:49 +0200

    fix routes and footer date

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	get 'landing_pages#index'
+	root 'landing_pages#index'
   get 'landing_pages/index'
   get 'landing_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
