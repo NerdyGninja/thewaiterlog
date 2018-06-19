f7e90374bbae103fa8686cc971b7e6eab446e178
Jacques le Roux <nerdy.gninja@gmail.com>
2018-06-19 19:02:09 +0200

    modified tips form

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -4,4 +4,8 @@ class Tip < ApplicationRecord
   scope :amount_desc, -> { order(amount: :desc) }
   scope :amount_asc, -> { order(amount: :asc) }
 
+  def number_to_currency_zar
+  	number_to_currency(number, :unit => "R", :separator => ",", :delimiter => ".")
+  end
+  
 end
